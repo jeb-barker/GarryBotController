295bd712f19f7047a9f8ab5e2fe4669f989dc7fe
Jeb Barker <48389379+jeb-barker@users.noreply.github.com>
2023-11-09 17:17:34 -0500

    added GPIO output code

--- app.py
+++ app.py
@@ -6,6 +6,9 @@ app = Flask(__name__)
 leftMotorStatus = False
 rightMotorStatus = False
 
+PIN_RIGHT = 0
+PIN_LEFT = 0
+
 @app.route('/controller', methods=['GET'])
 def controller():
     return render_template("joy.html")
@@ -26,9 +29,11 @@ def turnLeft():
     if not rightMotorStatus:
         # right motor should be on.
         print("right motor on")
+        GPIO.output(PIN_RIGHT, GPIO.HIGH)
     if leftMotorStatus:
         # left motor is on when it shouldn't be, update accordingly.
         print("left motor off")
+        GPIO.output(PIN_LEFT, GPIO.LOW)
 
     return "Turn Left"
 
@@ -38,9 +43,11 @@ def turnRight():
     if rightMotorStatus:
         # right motor should be off.
         print("right motor off")
+        GPIO.output(PIN_RIGHT, GPIO.LOW)
     if not leftMotorStatus:
         # left motor is off when it shouldn't be.
         print("left motor on")
+        GPIO.output(PIN_LEFT, GPIO.HIGH)
     return "Turn Right"
 
 
@@ -49,9 +56,11 @@ def zero():
     if rightMotorStatus:
         # right motor should be off.
         print("right motor off")
+        GPIO.output(PIN_RIGHT, GPIO.LOW)
     if leftMotorStatus:
         # left motor should be off.
         print("left motor off")
+        GPIO.output(PIN_LEFT, GPIO.LOW)
     return "Stop Movement"
 
 
@@ -60,7 +69,9 @@ def forward():
     if not rightMotorStatus:
         # right motor is off when it shouldn't be.
         print("right motor on")
+        GPIO.output(PIN_RIGHT, GPIO.HIGH)
     if not leftMotorStatus:
         # left motor is off when it shouldn't be.
         print("left motor on")
+        GPIO.output(PIN_LEFT, GPIO.HIGH)
     return "Forward"
